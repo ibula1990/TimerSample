9fa39d34f96941ade0193b4c38cdf355e4e08779
lenovo <lenovo@DESKTOP-BLIQA1L>
2021-03-23 00:34:36 -0700

    Add project files.

---
+++ TimerSample/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TimerSample
+{
+    partial class timerSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(116, 79);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 13);
+            this.lblName.TabIndex = 0;
+            // 
+            // timerSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(270, 196);
+            this.Controls.Add(this.lblName);
+            this.Name = "timerSample";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer Name Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ TimerSample/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerSample
+{
+    public partial class timerSample : Form
+    {
+        int a = 0;
+        
+        public timerSample()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (a == 0)
+            {
+                this.lblName.Text = "Ibu";
+                a++;
+            }
+            else if (a == 1)
+            {
+                this.lblName.Text = "Akhila";
+                a++;
+            }
+            else if (a == 2)
+            {
+                this.lblName.Text = "arun";
+                a++;
+            }
+            else if (a == 3)
+            {
+                this.lblName.Text = "mila";
+                a++;
+            }
+            else if (a == 4)
+            {
+                this.lblName.Text = "indu";
+                a++;
+            }
+            else
+            {
+                this.lblName.Text = "";
+                this.BackColor = Color.Red;
+
+            }
+        }
+    }
+}
